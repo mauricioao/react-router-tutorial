617ea53b94cfb5800348df06a5fd0ea6ae3aabb7
Mauricio Aguirre <mauricio.aguirreo@outlook.com>
2024-02-20 20:37:53 -0500

    URL Params in Loaders

--- src/main.jsx
+++ src/main.jsx
@@ -5,19 +5,22 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css'
-import Root from './routes/root';
 import ErrorPage from './error-page';
-import Contact from './routes/contact';
+import Contact, { loader as contactLoader } from './routes/contact';
+import Root, { loader as rootLoader, action as rootAction } from "./routes/root";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
       }
     ]
   },
